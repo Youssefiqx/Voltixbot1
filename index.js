98016ab6c806c329c3448a04c65f598d2f2bdf3a
Youssefiqx <yousseficonests100@gmail.com>
2025-08-28 22:51:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-const Discord = require('discord.js-selfbot-v13');
-const client = new Discord.Client({
-  readyStatus: false,
-  checkUpdate: false
-});
-//environment
-require('dotenv').config()
-
-function formatTime() { 
-  const date = new Date();
-  const options = {
-    timeZone: 'America/New_York', 
-    hour12: true,
-    hour: 'numeric',
-    minute: 'numeric'
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
-}
-const express = require("express")
-const app = express();
-var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
-app.get('/', (req, res) => {
-  res.send(`
-  <body>
-  <center><h1>Bot 24H ON!</h1></center
-  </body>`)
-});
 client.on('ready', async () => {
   console.clear();
   console.log(`${client.user.tag} - rich presence started!`);
@@ -40,16 +10,15 @@ client.on('ready', async () => {
     .setName('quaaxz')
     .setDetails(`Nitro is now`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1041035673118965772/1270521845841657907/image_2.webp?ex=66b4012d&is=66b2afad&hm=c0fa475d23f70fc777bcea2e70d9682a9aedf2d565ae549e64552ac303361d2b&=&format=webp&width=696&height=379') //You can put links in tenor or discord and etc.
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/1041035673118965772/1270521845841657907/image_2.webp')
     .setAssetsLargeText('Nitro') 
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1041035673118965772/1270522062095781990/checked.png?ex=66b40160&is=66b2afe0&hm=1413e3f740030479e77899e2e3bebeb05f97a80e7c0b828e6ec6e9012f86255d&=&format=webp&quality=lossless&width=768&height=768') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1041035673118965772/1270522062095781990/checked.png')
     .setAssetsSmallText('Small Text') 
     .addButton('Google', 'https://google.com');
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
-
+  client.user.setPresence({ status: "dnd" }); 
 });
 
-const mySecret = process.env['TOKEN'];
-client.login(mySecret);
+// هنا تحط التوكن مباشرة
+client.login("MTIyMDc1NzYwODM0NDg1MDU0Mg.GJ5wk_.Bk5XyuHv7jBC2hm8wkyKteBTEI5lY5QnCbqv9k");
